8ebc1e73ea9ce031a1b136a2b7a112c3349398aa
Ifeanyi Ibeh <reedbreeddigital@gmail.com>
2026-08-21 07:19:16 +0100

    fix(pixel): correct initialization sequence for meta pixel

--- frontend/src/pages/JourneyPlannerPage.tsx
+++ frontend/src/pages/JourneyPlannerPage.tsx
@@ -37,25 +37,24 @@ export function JourneyPlannerPage() {
       } catch (e) {}
 
       if (pixelId) {
-        if (!(window as any).fbq) {
-          ;(function(f: any, b: any, e: any, v: any, n?: any, t?: any, s?: any) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-              n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s);
-          })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-          ;(window as any).fbq('init', pixelId);
-        }
+        ;(function(f: any, b: any, e: any, v: any, n?: any, t?: any, s?: any) {
+          if (f.fbq) return;
+          n = f.fbq = function() {
+            n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+          };
+          if (!f._fbq) f._fbq = n;
+          n.push = n;
+          n.loaded = !0;
+          n.version = '2.0';
+          n.queue = [];
+          t = b.createElement(e);
+          t.async = !0;
+          t.src = v;
+          s = b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t, s);
+        })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+        
+        ;(window as any).fbq('init', pixelId);
         ;(window as any).fbq('track', 'PageView');
       }
     }
